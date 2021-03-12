c4f744cbc51c9548558e0e8787d7e917b8548094
Hristo Karchev <x_karchev@mail.bg>
2021-03-12 18:51:02 +0200

    Exam prep added
    Exam skeleton

---
+++ JS-App/10.ExamPrep/perfectSkeleton/scripts/db-init.js
@@ -0,0 +1,11 @@
+  var firebaseConfig = {
+      apiKey: "AIzaSyATN2kRCTHSOkWLt-_mnxj47IHFkDvTvqQ",
+      authDomain: "exercise-movies.firebaseapp.com",
+      databaseURL: "https://exercise-movies.firebaseio.com",
+      projectId: "exercise-movies",
+      storageBucket: "exercise-movies.appspot.com",
+      messagingSenderId: "884519328421",
+      appId: "1:884519328421:web:e9b81249b1bc92e7ff0b4c",
+  };
+  
+  firebase.initializeApp(firebaseConfig);
